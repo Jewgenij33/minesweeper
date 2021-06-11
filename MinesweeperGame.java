70d02c804084a6668fc04b000879c4c88ff4f623
Jewgenij33 <jewgenijszastalo@gmail.com>
2021-06-11 22:39:40 +0200

    dwl all project. Create methods restart, win and setScore

--- MinesweeperGame.java
+++ MinesweeperGame.java
@@ -5,6 +5,7 @@ import com.javarush.engine.cell.Game;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
@@ -12,6 +13,8 @@ public class MinesweeperGame extends Game {
     private int countMinesOnField;
     private int countFlags;
     private boolean isGameStopped;
+    private int countClosedTiles = SIDE * SIDE;
+    private int score;
 
     private static final String MINE = "\uD83D\uDCA3";
     private static final String FLAG = "\uD83D\uDEA9";
@@ -31,11 +34,11 @@ public class MinesweeperGame extends Game {
                 }
                 gameField[y][x] = new GameObject(x, y, isMine);
                 setCellColor(x, y, Color.ANTIQUEWHITE);
+                setCellValue(x, y, "");
             }
         }
         countMineNeighbors();
         countFlags = countMinesOnField;
-        isGameStopped = false;
     }
 
 
@@ -55,32 +58,45 @@ public class MinesweeperGame extends Game {
     @Override
     public void onMouseLeftClick(int x, int y) {                           //extended method which clicked mouse
             super.onMouseLeftClick(x, y);
+        if(isGameStopped)
+            restart();
+        else
             openTile(x, y);
+
         }
 
     private void openTile(int x, int y) {                                  //method which opening cell
         GameObject gameObject = gameField[y][x];
 
         if (!gameObject.isOpen && !gameObject.isFlag && !isGameStopped) {
-                    gameObject.isOpen = true;
-                    setCellColor(x, y, Color.AQUAMARINE);
-                    if (gameObject.isMine) {                                          //method changed: when opening a cell with a mine, the method is called gameOver();
-                        setCellValueEx(gameObject.x, gameObject.y, Color.RED, MINE);
-                        gameOver();
+            gameObject.isOpen = true;
+            countClosedTiles--;
+            setCellColor(x, y, Color.AQUAMARINE);
 
-                    } else if (gameObject.countMineNeighbors == 0) {
-                        setCellValue(gameObject.x, gameObject.y, "");
-                        List<GameObject> neighbors = getNeighbors(gameObject);
-                        for (GameObject neighbor : neighbors) {
-                            if (!neighbor.isOpen) {
-                                openTile(neighbor.x, neighbor.y);
-                            }
+            if (countClosedTiles > countMinesOnField) {                           //works until the number of closed cells is more than the number of mines
+                if (gameObject.isMine) {                                          //method changed: when opening a cell with a mine, the method is called gameOver();
+                    setCellValueEx(gameObject.x, gameObject.y, Color.RED, MINE);
+                    gameOver();
+                    countClosedTiles++;
+
+                } else if (gameObject.countMineNeighbors == 0) {                    // if there are no mines nearby, recursion is called
+                    setCellValue(gameObject.x, gameObject.y, "");
+                    List<GameObject> neighbors = getNeighbors(gameObject);
+                    for (GameObject neighbor : neighbors) {
+                        if (!neighbor.isOpen) {
+                            openTile(neighbor.x, neighbor.y);
+                            score += 5;
                         }
-                    } else {
-                        setCellNumber(gameObject.x, gameObject.y, gameObject.countMineNeighbors);
                     }
+                } else {
+                    setCellNumber(gameObject.x, gameObject.y, gameObject.countMineNeighbors);   //shows the number of mines in the adjacent cell
+                    score += 5;
                 }
-            }
+                setScore(score);
+            } else if (countClosedTiles == countMinesOnField && !gameObject.isMine)             //if the number of mines is comparable to the number of closed cells,
+                win();                                                                          // the game is stopped and the winner is declared
+        }
+    }
 
 
     @Override
@@ -112,6 +128,22 @@ public class MinesweeperGame extends Game {
     private void gameOver(){                                    //added a new method that ends the game and sets the flag "isGameStopped" to true
         isGameStopped = true;
         showMessageDialog(Color.RED, "Game failed!", Color.BROWN, 100);
+    }
+
+    private void win(){                                         // method that stops the game if you win
+        isGameStopped = true;
+        showMessageDialog(Color.GREEN, "congratulations".toUpperCase(Locale.ROOT), Color.WHITE, 40);
+    }
+
+    private void restart(){                                     //added restart method and conditions for its execution
+        isGameStopped = false;
+        countFlags = 0;
+        score = 0;
+        countMinesOnField = 0;
+        countClosedTiles = SIDE * SIDE;
+        setScore(score);
+        createGame();
+
     }
 
     private List<GameObject> getNeighbors (GameObject gameObject) {        //add neighbors cell on field
